67d9f1c2f51d24e3faa53ebf0d9018e09ef34a85
IIPeteII <pera17ab@student.cbs.dk>
2023-01-15 19:46:23 +0100

    commit text

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
 st.title(':moneybag: FINANCE APP :moneybag:')
-st.write("This app can be use to analyze stocks, portfolios and other things in the world of finance\n Check the sidebar for all the modules that can be used \n This is currently a work in progress")
+#st.image()
+st.markdown('This is an app that you can use to analyze stocks, portfolios and other things in the world of finance\
+    Use the sidebar for all the modules\
+    This is a work in progress app.')
 
