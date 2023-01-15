47772ee3408ba129eaf834b6c75f9ad5eb7cd95f
IIPeteII <pera17ab@student.cbs.dk>
2023-01-15 20:22:28 +0100

    text works?

--- app.py
+++ app.py
@@ -29,5 +29,6 @@ image_url = requests.get('https://raw.githubusercontent.com/IIPeteII/Finance/mai
 app_image = Image.open(BytesIO(image_url.content))
 st.image(app_image)
 
-st.markdown('This is an app that you can use to analyze stocks, portfolios and other things in the world of finance. Use the sidebar for all the modules. This is a work in progress app.')
-
+st.markdown('This is an app that _regonizes_ and **detects** license plates in images, then return an image of the car model.\
+    The app takes the string of the license plate and searches for it in the Danish license plate database, which then returns information such as brand, model and year.\
+    Thereafter, the app goes through a large dataset of car models and returns an image of the car, if the car is not in the dataset - a google search is performed.')
