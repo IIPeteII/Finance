42243e01af7e82e5b7607cdf54ad2762de5dbeaf
IIPeteII <pera17ab@student.cbs.dk>
2023-01-15 18:12:15 +0100

    app purpose

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.title(':moneybag: FINANCE APP :moneybag:')
-st.markdown('this app can be used to analyze stocks, portfolios and other things in the world of finance')
+st.write("This app can be use to analyze stocks, portfolios and other things in the world of finance\n Check the sidebar for all the modules that can be used \n This is currently a work in progress")
 
