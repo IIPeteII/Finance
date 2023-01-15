94d677959a767db6593d60b6e44c524ebc16c445
IIPeteII <pera17ab@student.cbs.dk>
2023-01-15 20:18:53 +0100

    gold bull?

--- app.py
+++ app.py
@@ -25,11 +25,11 @@ import streamlit as st #app deployment
 st.title(':moneybag: FINANCE APP :moneybag:')
 
 #image
-image_url = requests.get('https://raw.githubusercontent.com/IIPeteII/Finance/main/images/big-ass-bull.jpg')
+image_url = requests.get('')
 app_image = Image.open(BytesIO(image_url.content))
 st.image(app_image)
 
-st.markdown('This is an app that you can use to analyze stocks, portfolios and other things in the world of finance\
-    Use the sidebar for all the modules\
-    This is a work in progress app.')
+st.markdown('This is an app that you can use to analyze stocks, portfolios and other things in the world of finance\n
+    'Use the sidebar for all the modules\n
+    'This is a work in progress app.')
 
